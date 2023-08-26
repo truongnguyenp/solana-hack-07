852046bbdb7d81ac51f0e83d297d58b1a0a52ec4
Truong Nguyen <truongnguyen@team.enouvo.com>
2023-08-26 16:20:46 +0700

    feat: finish game screen

--- src/components/GameRow.tsx
+++ src/components/GameRow.tsx
@@ -10,18 +10,18 @@ type Props = {
 export function GameRow({game, onPress }: Props) {
   return (
     <Pressable onPress={() => onPress?.(game.id)} style={styles.container}>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View style={{  alignItems: "center" }}>
         <Image source={{ uri: game.team1.logo }} style={styles.image} />
         <Text style={styles.name}>{game.team1.name}</Text>
       </View>
 
       <View style={{ flexDirection: "row", alignItems: "center", gap: "4px" }}>
       <Text style={styles.score}>{game.team1.score}</Text>
-      <Divider orientation="vertical" width={5} />
+      <Text style={styles.score}> - </Text>
       <Text style={styles.score}>{game.team2.score}</Text>
       </View>
 
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View style={{ alignItems: "center" }}>
         <Image source={{ uri: game.team2.logo }} style={styles.image} />
         <Text style={styles.name}>{game.team2.name}</Text>
       </View>
@@ -36,27 +36,39 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingVertical: 16,
     paddingHorizontal: 20,
-    borderBottomWidth: 1,
-    backgroundColor: '#D6FFE5',
-    borderColor: "#E0E0E0",
+    backgroundColor: '#272639',
+    borderRadius: 8,
+
+    elevation: 3,
+
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 1,
+    shadowRadius: 8,
+    // borderColor: "#E0E0E0",
+    height: 100,
   },
   image: {
-    width: 44,
-    height: 44,
-    borderRadius: 22,
-    marginRight: 12,
+    width: 50,
+    height: 50,
+    // borderRadius: 22,
+    // marginRight: 12,
   },
   name: {
     fontSize: 16,
-    fontWeight: "500",
+    fontWeight: "600",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
   },
   score: {
-    fontSize: 18,
+    fontSize: 20,
+    color: "white",
+    fontWeight: "800",
   },
   divider: {
-    width: 1,
-    height: 16,
-    backgroundColor: "#E0E0E0",
-    marginHorizontal: 12,
+    fontSize: 20,
+    color: "white",
+    fontWeight: "800",
   },
 });
