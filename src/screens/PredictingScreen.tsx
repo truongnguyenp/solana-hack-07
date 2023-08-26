4ae37024adfbb84a52ed1a48d52ca47b7f4dfd50
Truong Nguyen <truongnguyen@team.enouvo.com>
2023-08-27 00:50:30 +0700

    feat: add predicting screen

--- src/screens/PredictingScreen.tsx
+++ src/screens/PredictingScreen.tsx
@@ -20,7 +20,7 @@ function LearnMoreLink({ url }: { url: string }) {
   return <Text onPress={() => Linking.openURL(url)}>Learn more</Text>;
 }
 
-export function ExamplesScreens() {
+export function PredictingScreens() {
   const [future, setFuture] = useRecoilState(testAtom);
 
   const didLaunch = useDidLaunch();
